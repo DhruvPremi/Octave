fe912bf712a0058de94e5e52da6a39ba465eb99c
Dhruv Premi <dhruvpremi@gmail.com>
2015-04-06 18:42:53 +0530

    Adding Structure examples

---
+++ ExampleToBoot/octave_structure.m
@@ -0,0 +1,9 @@
+% A Typical Structure created with name/value pairs
+sai = struct('first' , [1 2 3] , 'second' , 'Dhruv')
+
+sai.first(2)
+sai.second
+
+% Changing the Fields
+sai.second = 'Premi';
+sai
